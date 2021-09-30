0ee4a3f9e8483557a75c38eb525773446e1e5858
Michael Kimeu <mikekimsy18@gmail.com>
2021-09-30 14:23:49 +0300

    Including try catch block in the method

--- Adminbookinventory.aspx.cs
+++ Adminbookinventory.aspx.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Windows;
 
 namespace ELibraryManagement
 {
@@ -19,22 +22,16 @@ namespace ELibraryManagement
         }
         
         //add button click
-        protected void Button2_Click(object sender, EventArgs e)
+        protected void Button2_Click1(object sender, EventArgs e)
         {
-            //create a new connection to the database
-            SqlConnection con = new SqlConnection(strcon);
-            if(con.State== ConnectionState.Closed)
+            if (CheckIfBookExists())
             {
-                con.Open();
+                Response.Write("<script>alert('Book already exists')");
             }
-
-            string insertQuery = "insert into book_master_tbl values(@book_id,@book_name,@genre,@author_name,@publisher_name,@publisher_date,@language,@edition," +
-                "book_cost,@no_of_pages,@book_description,@actual_stock,@current_stock,@book_img_link,)";
-            SqlCommand cmd = new SqlCommand(insertQuery, con);
-
-            cmd.Parameters.AddWithValue("@book_id", TextBox1.Text.Trim());
-            cmd.Parameters.AddWithValue("@bool_name", TextBox2.Text.Trim());
-            //cmd.Parameters.AddWithValue("@genre", );
+            else
+            {
+                AddBook();
+            }
         }
 
         //update button click
@@ -55,6 +52,36 @@ namespace ELibraryManagement
 
         }
         //user defined methods
+        bool CheckIfBookExists()
+        {
+            try
+            {
+                //create a connection to the database
+                var con = new SqlConnection(strcon);
+                if(con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                //query the database
+                SqlCommand cmd = new SqlCommand("select * from book_master_tbl where book_id=' "+TextBox1.Text.Trim()+"' ", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else return false;
+
+
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + " ')</script>");
+                return false;
+            }
+        }
         void FillAuthorPublisherDetails()
         {
             //create a SqlConnection object
@@ -85,5 +112,61 @@ namespace ELibraryManagement
             DropDownList2.DataBind();
 
         }
+        void AddBook()
+        {
+            try
+            {
+                //adding the selected items on the listbox as a concatenated string to the database 
+                string genres = "";
+                //GetSelectedItems() returns an array of integers
+                foreach (int i in ListBox1.GetSelectedIndices())
+                {
+                    genres = genres + ListBox1.Items[i] + ",";
+                }
+                //removing the trailing comma
+                genres = genres.Remove(genres.Length - 1);
+
+                //adding input from the FileUpload1 control as a string to the database 
+                string filePath = "~/bookInventory/books1.png";
+                string fileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+                FileUpload1.SaveAs(Server.MapPath("bookInventory/" + fileName));
+                filePath = "~/bookInventory/" + fileName;
+
+                //create a new connection to the database
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                string insertQuery = "insert into book_master_tbl values(@book_id,@book_name,@genre,@author_name,@publisher_name,@publisher_date,@language,@edition," +
+                    "@book_cost,@no_of_pages,@book_description,@actual_stock,@current_stock,@book_img_link)";
+                SqlCommand cmd = new SqlCommand(insertQuery, con);
+
+                cmd.Parameters.AddWithValue("@book_id", TextBox1.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_name", TextBox2.Text.Trim());
+                cmd.Parameters.AddWithValue("@genre", genres);
+                cmd.Parameters.AddWithValue("@author_name", DropDownList3.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@publisher_name", DropDownList2.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@publisher_date", TextBox3.Text.Trim());
+                cmd.Parameters.AddWithValue("@language", DropDownList1.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@edition", TextBox4.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_cost", TextBox5.Text.Trim());
+                cmd.Parameters.AddWithValue("@no_of_pages", TextBox6.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_description", TextBox10.Text.Trim());
+                cmd.Parameters.AddWithValue("@actual_stock", TextBox7.Text.Trim());
+                cmd.Parameters.AddWithValue("@current_stock", TextBox7.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_img_link", filePath);
+
+                cmd.ExecuteNonQuery();
+                //con.Close();
+                Response.Write("<script>alert('Book details added successfully')</script>");
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" +ex.Message+ "')</script>");
+            }
+        }     
     }
 }
